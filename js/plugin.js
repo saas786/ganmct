7f27a5d9e92e4687c136aa2a4209f34a72f8b23c
Paul de Wouters <paul@desktop>
2013-02-08 14:59:55 -0300

    fixed js error

--- js/plugin.js
+++ js/plugin.js
@@ -1,14 +1,14 @@
 (function ($) {
 	"use strict";
 	$(function () {
-		$("body").on('click','.nav-menu a', function(){
+		$("body").on('click','.menu-item a', function(){
 			var trackingCode = $(this).next(".ga-tracking");
 			if(trackingCode.length > 0){
 				var t1 = trackingCode.data("tracking-1"),
 				    t2 = trackingCode.data("tracking-2"),
 				    t3 = trackingCode.data("tracking-3"),
 				    t4 = trackingCode.data("tracking-4");
-				_gaq.push([' + t1 + ', ' + t2 + ', ' + t3 + ', ' + t4 + ']);
+				_gaq.push([t1,t1,t3,t4]);
 			}
 
 		});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Google Analytics WordPress Nav Menu Event Tracking #
+
+A very long title for a simple plugin: what it does is insert a span element after the links in your navigation menus and also, adds 4 metaboxes for each menu item in the Custom Menus Interface.
+So for each menu item, you can configure what to track individually.
+When someone clicks a link in the navigation, an event listener will send the event to your Google Analytics.
